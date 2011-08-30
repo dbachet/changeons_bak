d38d88acf4e891b0fcf932d6bea28da92ad861df
Damien Bachet <onewaytodaf@gmail.com>
2011-08-30 22:36:14 +0200

    changed gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Changeons::Application.config.secret_token = 'd29ab6cc8eb477f3a7c741dbad939783fc667a7d4e45acd73792bfd8322799f34d152539dcee3b4421965007667a70c35673b98f3c0ab5f27b8b388cf538408a'
