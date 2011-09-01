06c96a4caefdb8a17a567a670a5ff34b5915c90c
Damien Bachet <onewaytodaf@gmail.com>
2011-09-01 18:20:49 +0200

    Added the devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Changeons::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  
+  # For devise
+  config.action_mailer.default_url_options = { :host => 'www.changeons.org' }
+  
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
