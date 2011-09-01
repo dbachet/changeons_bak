083d4c19a177f08f8539f47d4a5f8d67d020025c
Damien Bachet <onewaytodaf@gmail.com>
2011-09-01 18:33:58 +0200

    Added controller Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Changeons::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/search"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +56,7 @@ Changeons::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "Pages#home"
 
   # See how all your routes lay out with "rake routes"
 
