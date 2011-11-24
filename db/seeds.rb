97f943854900cb618c597a836633334c032d0949
Damien Bachet <onewaytodaf@gmail.com>
2011-11-24 12:18:35 +0100

    creates a first category and post type in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 # creates the first admin user 
-u = User.new(:email => "admin@changeons.org", :password => "adminadmin", :role => "admin")
+u = User.find_or_create_by_email(:email => "admin@changeons.org", :password => "adminadmin", :role => "admin")
 u.skip_confirmation!
 u.save(false)
+
+c = Category.find_or_create_by_name(:name => "Informatique")
+c.save
+
+t = PostType.find_or_create_by_name(:name => "Article")
+t.save
