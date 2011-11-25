ab66b6d660ad822ebddbe3ba9e013886267878a9
Damien Bachet <onewaytodaf@gmail.com>
2011-11-25 12:34:12 +0100

    upgraded fancybox to version 2.0.1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,43 +4,41 @@ $(document).ready(function() {
 	
 	// Fancyboxes
 	$('a.fancybox_signin').fancybox({
-	    'hideOnContentClick': false,
-		'onComplete':	function() {
+		closeClick: false,
+		afterLoad:	function() {
 			$('form#user_new[data-validate]').validate();
 		},
-		'onClosed': function() {
+		afterClose: function() {
 			$('form#new_reply[data-validate], form#new_comment[data-validate]').validate();
 		}
 	});
 	
 	$('a.new_category_link').fancybox({
-	    'hideOnContentClick': false,
-		'onComplete':	function() {
+		closeClick: false,
+		afterLoad:	function() {
 			$('form#new_category[data-validate]').validate();
-		},
-		'onClosed': function() {
-			// $('form#new_reply[data-validate], form#new_comment[data-validate]').validate();
 		}
 	});
 	
 	$('a.new_post_type_link').fancybox({
-	    'hideOnContentClick': false,
-		'onComplete':	function() {
+		closeClick: false,
+		afterLoad:	function() {
 			$('form#new_post_type[data-validate]').validate();
-		},
-		'onClosed': function() {
-			// $('form#new_reply[data-validate], form#new_comment[data-validate]').validate();
 		}
 	});
 	
 	$('a.index_categories_link, a.index_post_types_link').fancybox({
-	    'hideOnContentClick': false
+		closeClick: false
 	});
 	
 	// End fancyboxes
 
 var post_id = $('form#new_comment input#comment_title').data('post-id');
-$('a.fancybox_signin').attr('href', '/sessions/fancy_login?post_id=' + post_id);
+if (post_id){
+	$('a.fancybox_signin').attr('href', '/sessions/fancy_login?post_id=' + post_id);
+} else {
+	$('a.fancybox_signin').attr('href', '/sessions/fancy_login');
+}
 
 $('form#new_comment[data-validate]').validate();
 $('form#new_reply[data-validate]').validate();
