f632d0baa9344b6cc94db45a34204e1fc0aac9b2
Damien Bachet <onewaytodaf@gmail.com>
2012-02-03 08:55:16 +0100

    cleaned a bit the CSS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -82,7 +82,7 @@ $(document).ready(function() {
 	// End fancyboxes
 
 	// post list for home page
-	$('section#posts_home article').live({
+	$('section#posts article, section#tips article,	section#events article,	section#product_tests article, section#changeons_home article').live({
 	mouseenter: function() {
 	  	$('img', this).animate({
 	    	opacity: 1,
