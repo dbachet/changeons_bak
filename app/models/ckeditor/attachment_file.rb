1ed71552802264fc14d2126d9e5c91e673204b40
Damien Bachet <onewaytodaf@gmail.com>
2012-03-27 09:36:52 +0200

    added Amazon S3 support for ckeditor assets

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,8 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
-                    :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+                    :path => "/ckeditor_assets/attachments/:id/:filename",
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :url  => ":s3_eu_url"
   
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,9 +1,11 @@
 # -*- encoding : utf-8 -*-
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-	                  :styles => { :content => '800>', :thumb => '118x100#' }
+                    :path => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+	                  :styles => { :content => '800>', :thumb => '118x100#' },
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :url  => ":s3_eu_url"
 	
 	validates_attachment_size :data, :less_than => 2.megabytes
 	validates_attachment_presence :data
