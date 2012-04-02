26c3f9ad6a9f5e7eecad5d4d37c8c450a5c7560d
Damien Bachet <onewaytodaf@gmail.com>
2012-04-02 09:49:56 +0200

    Homogenized the CSS of titles

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,10 @@
 # -*- encoding : utf-8 -*-
 class TagsController < AuthorizedController
+  add_breadcrumb ("<div class='home_breadcrumbs'></div>").html_safe, :root_path, :title => "Revenir en page d'accueil"
   
   def show_posts
     @tag = ActsAsTaggableOn::Tag.find(params[:tag_id])
-    @posts = Post.tagged_with(@tag.name)
+    @posts = Post.tagged_with(@tag.name).page(params[:page]).per(5)
+    add_breadcrumb "Recherche par tag \"#{@tag.name}\"", ""
   end
 end
