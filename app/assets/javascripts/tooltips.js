d7b142e2febfa9e02e2fb90290a0f5eed7c8808f
Damien Bachet <onewaytodaf@gmail.com>
2012-04-14 18:49:25 +0200

    improved the CSS for the post form, the picture form is still to modify

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -3,4 +3,6 @@ $(document).ready(function(){
 	$('#back_to_top_button.tooltipped').tipsy({fade:true, live:true, offset: 8});
 	$('#display_categories_button.tooltipped').tipsy({fade:true, live:true, offset: -10});
 	$('#breadcrumbs a').tipsy({fade:true, live:true, offset: 0});
+	$('article.form textarea').tipsy({trigger: 'focus', gravity: 'e'});
+	$('article.form input[type="text"]').tipsy({trigger: 'focus', gravity: 'w'});
 });
