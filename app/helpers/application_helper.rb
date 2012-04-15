899449066c65ce57672021ab618450f2344ac41c
Damien Bachet <onewaytodaf@gmail.com>
2012-04-15 09:31:23 +0200

    added tooltips on form pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   
   def javascript(*files)
-      content_for(:javascript_includes) { javascript_include_tag(*files) }
+      content_for(:head) { javascript_include_tag(*files) }
   end
   
   def getMonthFromNumber(month)
